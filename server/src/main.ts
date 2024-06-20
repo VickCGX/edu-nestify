07e006c55471a4ba6f52c9c589ae69c34196d1c9
VickCGX <vicknguyencgx1992@gmail.com>
2024-06-20 10:37:30 +0700

    feat: implement nest swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Edu Nestify API')
+    .setDescription('API documentation for the Edu Nestify application')
+    .setVersion('1.0')
+    .addTag('users')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
