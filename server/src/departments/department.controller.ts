611049351b9e6cff00f0fa50e36855eb308a2f3f
VickCGX <vicknguyencgx1992@gmail.com>
2024-06-22 13:24:19 +0700

    feat: enhance department controller using api decorator

--- server/src/departments/department.controller.ts
+++ server/src/departments/department.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Post, Body, Put, Param, Get, Query, Delete, HttpCode, HttpStatus } from '@nestjs/common'
-import { ApiTags, ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger'
+import { Controller, Post, Body, Put, Param, Get, Query, Delete, HttpStatus } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 import { DepartmentService } from './department.service'
 import { CreateDepartmentDto } from './dtos/create-department.dto'
 import { UpdateDepartmentDto } from './dtos/update-department.dto'
 import { DepartmentDto } from './dtos/department.dto'
-import { FilterDto, PaginationDto, SortDto, PaginationResponse } from 'src/shared'
+import {
+  FilterDto,
+  PaginationDto,
+  SortDto,
+  PaginationResponse,
+  ApiResponseModel,
+  ApiResponseArrayModel,
+  ApiOperationSummary,
+  ApiQueryOptions,
+  HttpStatusCode
+} from 'src/shared'
 
 @ApiTags('departments')
 @Controller('departments')
@@ -12,36 +22,25 @@ export class DepartmentController {
   constructor(private readonly departmentService: DepartmentService) {}
 
   @Post()
-  @HttpCode(HttpStatus.CREATED)
-  @ApiOperation({ summary: 'Create department' })
-  @ApiResponse({ status: HttpStatus.CREATED, description: 'Department created successfully', type: DepartmentDto })
-  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Invalid input' })
-  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
+  @HttpStatusCode(HttpStatus.CREATED)
+  @ApiOperationSummary('Create department')
+  @ApiResponseModel(DepartmentDto, HttpStatus.CREATED)
   async create(@Body() createDepartmentDto: CreateDepartmentDto): Promise<DepartmentDto> {
     return this.departmentService.create(createDepartmentDto)
   }
 
   @Get(':id')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: 'Get department by ID' })
-  @ApiResponse({ status: HttpStatus.OK, description: 'Department retrieved successfully', type: DepartmentDto })
-  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
-  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Department not found' })
+  @HttpStatusCode(HttpStatus.OK)
+  @ApiOperationSummary('Get department by ID')
+  @ApiResponseModel(DepartmentDto, HttpStatus.OK)
   async findOneById(@Param('id') id: number): Promise<DepartmentDto> {
     return this.departmentService.findOne(id)
   }
 
   @Get()
-  @ApiOperation({ summary: 'Get many departments' })
-  @ApiQuery({ name: 'filter', type: FilterDto, required: false, description: 'Filter options' })
-  @ApiQuery({ name: 'pagination', type: PaginationDto, required: false, description: 'Pagination options' })
-  @ApiQuery({ name: 'sort', type: SortDto, required: false, description: 'Sort options' })
-  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'Return many departments.',
-    type: () => PaginationResponse<DepartmentDto>
-  })
+  @ApiOperationSummary('Get many departments')
+  @ApiQueryOptions()
+  @ApiResponseArrayModel(DepartmentDto, HttpStatus.OK)
   async findMany(
     @Query('filter') filterDto?: FilterDto,
     @Query('pagination') paginationDto?: PaginationDto,
@@ -51,20 +50,17 @@ export class DepartmentController {
   }
 
   @Put(':id')
-  @HttpCode(HttpStatus.OK)
-  @ApiOperation({ summary: 'Update department' })
-  @ApiResponse({ status: HttpStatus.OK, description: 'Department updated successfully', type: DepartmentDto })
-  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Invalid input' })
-  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Department not found' })
-  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
+  @HttpStatusCode(HttpStatus.OK)
+  @ApiOperationSummary('Update department')
+  @ApiResponseModel(DepartmentDto, HttpStatus.OK)
   async update(@Param('id') id: number, @Body() updateDepartmentDto: UpdateDepartmentDto): Promise<DepartmentDto> {
     return this.departmentService.update(id, updateDepartmentDto)
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: 'Delete a department' })
-  @ApiResponse({ status: HttpStatus.OK, description: 'Deleted successfully' })
-  @ApiResponse({ status: HttpStatus.FORBIDDEN, description: 'Forbidden.' })
+  @HttpStatusCode(HttpStatus.NO_CONTENT)
+  @ApiOperationSummary('Delete a department')
+  @ApiResponseModel(null, HttpStatus.NO_CONTENT)
   async remove(@Param('id') id: number): Promise<void> {
     return this.departmentService.remove(id)
   }
