af7f5cb1b164661b7f3e218507ff0f2acbce9c4b
VickCGX <vicknguyencgx1992@gmail.com>
2024-07-02 14:41:01 +0700

    fix: duplicated code

--- backend/src/materials/material/dtos/create-material.dto.ts
+++ backend/src/materials/material/dtos/create-material.dto.ts
@@ -1,33 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsInt, IsString, IsNotEmpty, IsOptional } from 'class-validator'
+import { OmitType } from '@nestjs/swagger'
+import { MaterialDto } from './material.dto'
 
-export class CreateMaterialDto {
-  @ApiProperty({ example: 'Introduction to Programming' })
-  @IsString()
-  @IsNotEmpty()
-  title: string
-
-  @ApiProperty({ example: 'Content of the material', required: false })
-  @IsString()
-  @IsOptional()
-  content?: string
-
-  @ApiProperty({ example: '/path/to/material.pdf' })
-  @IsString()
-  @IsNotEmpty()
-  path: string
-
-  @ApiProperty({ example: 1 })
-  @IsInt()
-  materialTypeId: number
-
-  @ApiProperty({ example: 1, required: false })
-  @IsInt()
-  @IsOptional()
-  subjectId?: number
-
-  @ApiProperty({ example: 1, required: false })
-  @IsInt()
-  @IsOptional()
-  moduleId?: number
-}
+export class CreateMaterialDto extends OmitType(MaterialDto, ['id', 'createdAt', 'updatedAt'] as const) {}
