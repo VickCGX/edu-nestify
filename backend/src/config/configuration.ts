f0e6fd2d4e8ff5d2b060609ebbbe0791ff749051
VickCGX <vicknguyencgx1992@gmail.com>
2024-07-04 09:18:33 +0700

    feat: add AWS s3 service

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -5,5 +5,10 @@ export default () => ({
   },
   jwt: {
     secret: process.env.JWT_SECRET
+  },
+  aws: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
   }
 })
